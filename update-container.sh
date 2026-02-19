e3a017ef29fc3732af4c39201b3c4a5cd2fea553
stephen.j.sheldon.ctr <stephen.j.sheldon.ctr@sclzg2.onmicrosoft.us>
2026-02-19 10:34:08 -0500

    cleanup

--- update-container.sh
+++
@@ -1,1 +0,0 @@
-docker stop adsb-cot-converter 2>/dev/null; docker rm adsb-cot-converter 2>/dev/null; docker build -t adsb-to-cot . && docker run -d --name adsb-cot-converter --restart unless-stopped adsb-to-cot
